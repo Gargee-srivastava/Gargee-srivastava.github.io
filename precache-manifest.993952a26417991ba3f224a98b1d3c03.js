472f69a3b365f947513024f5e907dffd667fc172
Gargee-srivastava <srivastava.gargee@gmail.com>
2020-08-25 18:56:15 +0530

    Updates

---
+++ precache-manifest.993952a26417991ba3f224a98b1d3c03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63cc669d341ca392fbb6e3b23fdc71c2",
+    "url": "/Gargee-srivastava/Gargee-srivastava.github.io/index.html"
+  },
+  {
+    "revision": "2482a0c7b24e53b83fc0",
+    "url": "/Gargee-srivastava/Gargee-srivastava.github.io/static/css/main.9ea3f718.chunk.css"
+  },
+  {
+    "revision": "ed327bc5312e9a74213d",
+    "url": "/Gargee-srivastava/Gargee-srivastava.github.io/static/js/2.d795e081.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Gargee-srivastava/Gargee-srivastava.github.io/static/js/2.d795e081.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2482a0c7b24e53b83fc0",
+    "url": "/Gargee-srivastava/Gargee-srivastava.github.io/static/js/main.ff2f3ac8.chunk.js"
+  },
+  {
+    "revision": "1b8f3c2c963ab231eb68",
+    "url": "/Gargee-srivastava/Gargee-srivastava.github.io/static/js/runtime-main.48c0393f.js"
+  }
+]);
